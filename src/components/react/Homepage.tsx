257378af5a134982ec988c922886693cc575d19d
Hung Quang Thieu <hqthieu@gmail.com>
2025-05-26 01:41:32 +0200

    add social links

--- src/components/react/Homepage.tsx
+++ src/components/react/Homepage.tsx
@@ -1,9 +1,17 @@
-import "../../styles/global.css";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 
 export default function Homepage() {
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center bg-[#0b0e13]">
-      <h1 className="text-white fold-bold text-5xl">Helo, I'm Hung ✌️</h1>
+      <h1 className="text-white fold-bold text-5xl mb-8">Helo, I'm Hung ✌️</h1>
+      <div className="flex w-14 justify-between">
+        <a href="https://github.com/hungqt/" target="_blank">
+          <GitHubLogoIcon color="white" width={20} height={20} />
+        </a>
+        <a href="https://www.linkedin.com/in/hungthieu/" target="_blank">
+          <LinkedInLogoIcon color="white" width={20} height={20} />
+        </a>
+      </div>
     </div>
   );
 }
