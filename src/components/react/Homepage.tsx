ed6c51e98f1f2bc20e9c54df94a6e8b1711b68eb
Hung Quang Thieu <hqthieu@gmail.com>
2025-05-26 00:59:17 +0200

    Set up astro github actions deploy

---
+++ src/components/react/Homepage.tsx
@@ -0,0 +1,9 @@
+import "../../styles/global.css";
+
+export default function Homepage() {
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-[#0b0e13]">
+      <h1 className="text-white fold-bold text-5xl">Helo, I'm Hung ✌️</h1>
+    </div>
+  );
+}
